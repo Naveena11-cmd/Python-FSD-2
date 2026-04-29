889c318c26323cfa3766fa994765c9a1477fdb0e
Naveena <naveenaadvani@gmail.com>
2026-04-29 12:31:07 +0530

    Update m2.js

--- ch-6-multer/m2.js
+++ ch-6-multer/m2.js
@@ -7,7 +7,7 @@ var store=m.diskStorage({destination:"my",filename:(req,file,cb)=>
     cb(null,file.originalname)
     console.log(file)
 }})
-var upload=m({storage:store})
+var upload=m({storage:store, limits:{fileSize:1024*1024*10}})
 app.post("/upload",upload.array("mydoc",3),(req,res)=>{  //3 is the max number of files user can upload
     var file=req.files;
     console.log(file);
