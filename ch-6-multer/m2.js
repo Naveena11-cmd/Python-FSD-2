abcd097ee3c0f5dbf7d55281462d084879758489
Naveena <naveenaadvani@gmail.com>
2026-04-29 11:49:34 +0530

    Refactor upload endpoint and change listening port
    Updated the file upload functionality and changed the server port.

--- ch-6-multer/m2.js
+++ ch-6-multer/m2.js
@@ -1,26 +1,25 @@
-var express = require("express");
-var app = express();
-var m = require("multer");
-
-app.use(express.static(__dirname, { index: "m2.html" }))
-var store = m.diskStorage({
-    destination: "my", filename: (req, file, cb) => {
-        cb(null, file.originalname)
-        console.log(file);
-    }
-})
-
-var upload = m({ storage: store })
-app.post("/upload", upload.array("mydoc",3), (req, res) => {  //3 : number of files user needs to insert
-    var file = req.files;
+var express=require("express")
+var app=express();
+var m=require("multer");
+app.use(express.static(__dirname,{index:"m2.html"}))
+var store=m.diskStorage({destination:"my",filename:(req,file,cb)=>
+{
+    cb(null,file.originalname)
+    console.log(file)
+}})
+var upload=m({storage:store})
+app.post("/upload",upload.array("mydoc",3),(req,res)=>{  //3 is the max number of files user can upload
+    var file=req.files;
     console.log(file);
-
-    if (file) {
+    if(file)
+    {
         res.type("text/html")
-        for (f of file){
-            res.write(`<h1>File <span style="color:red"> ${f.originalname} </span> has been uploaded in folder ${f.destination[f]}</h1>`)
-        }
+       for (f of file)
+       {
+        res.write(`<h1> File ${f.originalname} has been uploaded </h1>`)
+      
+       }
     }
-    res.send();
+    res.send()
 })
-app.listen(7009);
+app.listen(7006)
